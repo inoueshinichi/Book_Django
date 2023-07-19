a330f6ab682ae0c04068c7d7b4795e0d7cee1f31
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-19 20:16:29 +0900

    [Update] make page

--- private_diary/private_diary/settings.py
+++ private_diary/private_diary/settings.py
@@ -126,6 +126,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'deploy') # 配信用のURL
+
+# 静的ファイルの保存場所を示す
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+    # BASE_DIR / 'static',
+]
+
+print('BaseDIR:', BASE_DIR)
+print('static', os.path.join(BASE_DIR, 'static'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -173,7 +184,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # }
 
 
-# 静的ファイルの保存場所を示す
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
