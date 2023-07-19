3cdae30797bbfdbca8fa553a605727299a9da8a3
Inoue Shinichi Macbook Pro 2016 <inoue.shinichi.1800@gmail.com>
2023-07-20 01:24:41 +0900

    [Update] chapter9 fin

--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -3,6 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 
+from django.urls import reverse_lazy # URLの逆引き
+from django.contrib import messages # ブラウザで応答 ERROR/WARNING/SUCCESS/INFO
+
+from .forms import InquiryForm
+
+import logging
+logger = logging.getLogger(__name__)
+
+
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
     
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    # override
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
---
+++ private_diary/private_diary/settings_dev.py
@@ -0,0 +1,55 @@
+"""開発環境専用のsettings.py
+"""
+from .settings_common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギング設定
+LOGGING = {
+    'version': 1, # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定　
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]'
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    },
+}
+
+
+# 開発時のメールの配信先設定(to console)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
